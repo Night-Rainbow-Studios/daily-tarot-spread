a207970ba47feaef621831bc10c87d9a4d2cce52
Diego Jonás Pérez <35743432+binarioGH@users.noreply.github.com>
2020-08-04 14:08:32 -0700

    cool animation

--- animation/animate.js
+++ animation/animate.js
@@ -2,17 +2,24 @@ var half_height = parseInt(window.screen.height / 2);
 var half_width = parseInt(window.screen.width / 2);
 
 var cards = [ 
-	document.getElementById("card3").classList.add("loaded"),
-	document.getElementById("card2").classList.add("loaded"),
-	document.getElementById("card1").classList.add("loaded")]; //Definir una lista con todas las cartas.
+	document.getElementById("card3"),
+	document.getElementById("card2"),
+	document.getElementById("card1")]; //Definir una lista con todas las cartas.
 var move = "0%";
 move = "300%"; //Definir cuanto se va a mover dependiendo de la pantalla.
 	if(window.screen.width > 1000){
 		move = "400%";
 	}
+
+
 cards.forEach(function(item, index){ //Mover las cartas
-	console.log(".")
+	setTimeout(()=>{	
+		item.style.transitionDuration = "100ms"; 
+		item.classList.add("loaded");
+		item.style.transform = "translate("+move+", 80%)";
+	}, index*300);
 });
+
 document.getElementById("redirect").classList.add("loaded");
 
 
