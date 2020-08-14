eb63f27844c76db23b3611af0527296cc395790f
Diego Jonás Pérez <35743432+binarioGH@users.noreply.github.com>
2020-08-13 18:25:24 -0700

    UX bug fixed

--- animation/compatibility.js
+++ animation/compatibility.js
@@ -5,9 +5,22 @@ var calculate = document.getElementById("calculate");
 var selected = document.getElementsByClassName("select-sign");
 
 var already_calculated = false;
+var sign1 = selected[0].value;
+var sign2 = selected[1].value;
+var lasts1 = "";
+var lasts2 = "";
 calculate.addEventListener("click", function(){
-	var sign1 = selected[0].value;
-	var sign2 = selected[1].value;
+	sign1 = selected[0].value;
+	sign2 = selected[1].value;
+	console.log(lasts1);
+	console.log(lasts2);
+	if(lasts1 != sign1 || lasts2 != sign2){
+		console.log("uf");
+		already_calculated = false;
+	}
+	lasts1 = sign1;
+	lasts2 = sign2;
+	
 	if(!already_calculated){
 		if((sign1 == "Gemini ♊" && sign2 == "Virgo ♍") || (sign2 == "Gemini ♊" && sign1 == "Virgo ♍")){
 			percentage.innerHTML = "10000%";
