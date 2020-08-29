c508f8965e6ce8c9cf0ab731ae7f3eee79f08755
Diego Jonás Pérez <35743432+binarioGH@users.noreply.github.com>
2020-08-29 13:20:58 -0700

    script fixed

--- sendfiles.py
+++ sendfiles.py
@@ -4,6 +4,7 @@ from os import listdir, chdir
 from os.path import isfile
 from sys import argv
 from pathlib import Path
+from optparse import OptionParser as opt
 
 def send(file, ftp):
 	filepath = Path(file)
@@ -12,29 +13,25 @@ def send(file, ftp):
 
 
 def main():
-	chdir("assets")
-	user = argv[1]
-	passw = argv[2]
-	server = "147.135.84.40"
-	client = FTP(server)
-	print("[+]Login into server")
-	client.login(user, passw)
-	client.cwd("/web/tarotgratis.best/public_html/assets")
-	count = 0
-	
-	while count < 2:
-		cf = 0 
-		print("[+]sending files")
-		if count == 1: 
-			chdir('big')
-			client.cwd("/web/tarotgratis.best/public_html/assets/big")
-		for file in listdir():
-			if isfile(file):
-				send(file, client)
-				cf += 1
-		print("[+]Total sent files in this directory: {}".format(cf))
-
-		count += 1
+	op = opt("Usage: %prog [flags] [values]")
+	op.add_option("-u", "--user", dest="user", help="Set the user name of the FTP server.")
+	op.add_option("-p", "--password", dest="passw", help="Set the password of the FTP server.")
+	op.add_option("-d", "--directory", dest="dir", default=".", help="Set the local directory where are the files that you want to send.")
+	op.add_option("-c", "--cwd", dest="cwd", default="/web/tarotgratis.best/public_html", help="Set the directory of the FTP server.")
+	op.add_option("-s", "--server", dest="server", default="147.135.84.40", help="Set the FTP server.")
+	(o, argv) = op.parse_args()
+	chdir(o.dir)
+	client = FTP(o.server)
+	print("[+]Login into {}".format(o.server))
+	client.login(o.user, o.passw)
+	client.cwd(o.cwd)
+	cf = 0 
+	print("[+]sending files")
+	for file in listdir():
+		if isfile(file):
+			send(file, client)
+			cf += 1
+	print("[+]Total sent files in this directory: {}".format(cf))
 
 
 
