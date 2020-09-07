6b1b38db5c057d3cc606506dcfa5fb755f014f06
Diego Jonás Pérez <35743432+binarioGH@users.noreply.github.com>
2020-09-06 18:22:27 -0700

    trying to implement the share button

--- animation/share.js
+++ animation/share.js
@@ -1,7 +1,10 @@
+console.log("loaded");
 var share = document.getElementById("shrbt");
 
 share.addEventListener("click", function(){
-	html2canvas(document.getElementById("your-cards")).then(function(canvas){
-		console.log(canvas.toDataURL("image/jpeg", 0.9));
-	});
-});
+	html2canvas(document.getElementById("your-cards")).then(
+		function(canvas){
+			document.body.appendChild(canvas);
+		}
+	);
+});	
