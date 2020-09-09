1f40f40b3bd4767bc3e63e23d127d9c62b6ee1cc
Daniel Rosas <56138779+danysora@users.noreply.github.com>
2020-09-08 22:50:42 -0500

    fixed bugs

---
+++ game.js
@@ -0,0 +1,23 @@
+FBInstant.initializeAsync()
+  .then(function () {
+      var progress = 0;
+      var interval = setInterval(function () {
+        progress += 3;
+        FBInstant.setLoadingProgress(progress);
+
+        if (progress >= 95) {
+          clearInterval(interval)
+          FBInstant.startGameAsync()
+            .then(function () {});
+          var contextId = FBInstant.context.getID();
+          var contextType = FBInstant.context.getType();
+
+          var playerName = FBInstant.player.getName();
+          var playerPic = FBInstant.player.getPhoto();
+          var playerId = FBInstant.player.getID();
+          document.getElementById("name").innerHTML = playerName;
+          game.start();
+        }
+      }, 100)
+      // Start loading game assets here
+    );
