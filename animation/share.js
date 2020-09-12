5ddb9ecf7638c47a1adbec96fcc91ff081e39db2
Diego Jonás Pérez <35743432+binarioGH@users.noreply.github.com>
2020-09-11 23:59:09 -0700

    Sharing

--- animation/share.js
+++ animation/share.js
@@ -4,7 +4,7 @@ var share = document.getElementById("shrbt");
 share.addEventListener("click", function(){
 	html2canvas(document.getElementById("your-cards")).then(
 		function(canvas){
-			document.body.appendChild(canvas);
+			console.log("UnU")
 		}
 	);
 });	
